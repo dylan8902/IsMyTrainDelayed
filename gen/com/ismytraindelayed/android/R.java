79f2e62e5a4380183d889b7d0ecb3ca46ebecddb
Dylan Jones <dyl@anjon.es>
2014-07-27 09:03:02 +0100

    build things

--- gen/com/ismytraindelayed/android/R.java
+++ gen/com/ismytraindelayed/android/R.java
@@ -22,21 +22,20 @@ public final class R {
     }
     public static final class id {
         public static final int LinearLayout02=0x7f070001;
-        public static final int ScrollView01=0x7f070007;
-        public static final int bottom=0x7f070009;
+        public static final int bottom=0x7f070008;
         public static final int button=0x7f070006;
-        public static final int delete_journey=0x7f07000c;
+        public static final int delete_journey=0x7f07000b;
         public static final int from=0x7f070004;
         public static final int main=0x7f070000;
-        public static final int nearest_from=0x7f07000f;
-        public static final int nearest_to=0x7f07000e;
+        public static final int nearest_from=0x7f07000e;
+        public static final int nearest_to=0x7f07000d;
         public static final int radio_arrivals=0x7f070003;
         public static final int radio_departures=0x7f070002;
-        public static final int results=0x7f070008;
-        public static final int save_journey=0x7f07000b;
-        public static final int saved_journeys=0x7f07000a;
+        public static final int results=0x7f070007;
+        public static final int save_journey=0x7f07000a;
+        public static final int saved_journeys=0x7f070009;
         public static final int to=0x7f070005;
-        public static final int top=0x7f07000d;
+        public static final int top=0x7f07000c;
     }
     public static final class layout {
         public static final int list_item=0x7f030000;
@@ -47,5 +46,9 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int destination_optional=0x7f040001;
+        public static final int from_required=0x7f040003;
+        public static final int into_required=0x7f040004;
+        public static final int origin_optional=0x7f040002;
     }
 }
